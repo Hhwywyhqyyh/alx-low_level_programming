370afb587c170cee811e38b6243c0565c3282662
Simange <simange97@gmail.com>
2023-02-19 10:06:59 -0800

    uzoreverser

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0; (success)
+ */
+
+int main(void)
+{
+	char  cs;
+
+	cs = 'z';
+		while
+			(cs >= 'a') {
+				putchar(cs);
+				cs--;
+			}
+	putchar('\n');
+	return (0);
+}
+
