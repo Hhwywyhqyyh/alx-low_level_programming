2fb9c2cb75ed527a4f3724421ba207c2aeb5308c
Simange <simange97@gmail.com>
2023-02-19 09:50:04 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * description: printing single digit numbers
+ *
+ * Return: Always 0:
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar (c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
